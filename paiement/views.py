d6f4466c8b31b45a3ba6ddc7337b977ecb8e42d6
Cherifa0204 <cogboneinoussa@gmail.com>
2024-04-24 08:20:49 +0000

    modification des formules de calcul  sur le total cnss du compte bancaire

--- paiement/views.py
+++ paiement/views.py
@@ -471,63 +471,6 @@ def create_compte(request, id=0):
 
 
 
-# @login_required(login_url=settings.LOGIN_URL)
-# def compte_bancaire(request, id_annee_selectionnee):
-#     """
-#     Affiche les informations du compte bancaire pour une année universitaire donnée.
-
-#     :param request: L'objet HttpRequest utilisé pour effectuer la requête.
-#     :param id_annee_selectionnee: L'ID de l'année universitaire sélectionnée.
-#     :return: Un objet HttpResponse contenant le rendu de la page 'compte_bancaire/compte_bancaire.html'.
-#     :raises: Http404 si l'année universitaire sélectionnée n'est pas trouvée.
-
-#     Cette vue récupère les frais associés à l'année universitaire spécifiée,
-#     puis les renvoie au template 'compte_bancaire/compte_bancaire.html' pour affichage.
-#     """
-#     semestres_annee = Semestre.objects.filter(annee_universitaire=id_annee_selectionnee)
-#     etudiants_annee = Etudiant.objects.filter(semestres__in=semestres_annee)
-#     paiements_annee = Paiement.objects.filter(etudiant__in=etudiants_annee)
-#     montant_total_paiements = paiements_annee.aggregate(Sum('montant'))['montant__sum'] or 0
-
-#     salaires = Salaire.objects.filter(annee_universitaire=id_annee_selectionnee)
-#     total_salaire_brut = sum(salaire.personnel.salaireBrut + salaire.prime_efficacite + salaire.prime_qualite + salaire.frais_travaux_complementaires+ salaire.prime_anciennete for salaire in salaires)
-#     total_cnss = sum(salaire.frais_prestations_familiale_salsalaire * salaire.personnel.salaireBrut + salaire.frais_prestations_familiales * salaire.personnel.salaireBrut + salaire.frais_risques_professionnel * salaire.personnel.salaireBrut + salaire.frais_pension_vieillesse_emsalaire * salaire.personnel.salaireBrut for salaire in salaires)
-
-#     total_cnss = sum(salaire.frais_prestations_familiale_salsalaire * salaire.personnel.salaireBrut + salaire.frais_prestations_familiales * salaire.personnel.salaireBrut + salaire.frais_risques_professionnel * salaire.personnel.salaireBrut + salaire.frais_pension_vieillesse_emsalaire * salaire.personnel.salaireBrut for salaire in salaires)
-#     total_irpp = sum(salaire.irpp + salaire.tcs for salaire in salaires)
-#     total_salaire_net = sum(salaire.salaire_net_a_payer for salaire in salaires)
-#     montant_total_salaires = sum(salaire.salaire_net_a_payer + (salaire.frais_prestations_familiale_salsalaire * salaire.personnel.salaireBrut + salaire.frais_prestations_familiales * salaire.personnel.salaireBrut + salaire.frais_risques_professionnel * salaire.personnel.salaireBrut + salaire.frais_pension_vieillesse_emsalaire * salaire.personnel.salaireBrut + salaire.tcs)for salaire in salaires)
-
-#     fournisseurs = Fournisseur.objects.filter(annee_universitaire=id_annee_selectionnee)
-#     total_paiements_fournisseurs = sum(fournisseur.montant for fournisseur in fournisseurs)
-
-#     fiche_de_paies = FicheDePaie.objects.filter(annee_universitaire=id_annee_selectionnee)
-#     total_fiche_de_paies = sum(fiche_de_paie.montant for fiche_de_paie in fiche_de_paies)
-
-#     fiche_de_charge = Charge.objects.filter(annee_universitaire=id_annee_selectionnee)
-#     total_charges = sum(fiche.montant for fiche in fiche_de_charge)
-
-#     total_salaires = (total_charges + total_fiche_de_paies) + montant_total_salaires
-
-#     compte_bancaire_obj = CompteBancaire.objects.first()
-#     compte_existe = compte_bancaire_obj is not None
-
-#     context = {
-#         'id_annee_selectionnee': id_annee_selectionnee,
-#         'total_paiements': montant_total_paiements,
-#         'montant_total_salaires': montant_total_salaires,
-#         'compte_bancaire_obj': compte_bancaire_obj,
-#         'total_salaire_brut': total_salaire_brut,
-#         'total_cnss': total_cnss,
-#         'total_irpp': total_irpp,
-#         'total_salaire_net': total_salaire_net,
-#         'total_paiements_fournisseurs': total_paiements_fournisseurs,
-#         'total_fiche_de_paies': total_fiche_de_paies,
-#         'total_charges': total_charges,
-#         'total_salaires': total_salaires,
-#         'compte_existe': compte_existe,
-#     }
-#     return render(request, 'compte_bancaire/compte_bancaire.html', context)
 
 
 @login_required(login_url=settings.LOGIN_URL)
