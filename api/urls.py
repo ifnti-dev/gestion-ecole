6c0c240318ccc568c71ee691f8fd83b405589293
Astronaute7 <bessanedouard1@gmail.com>
2024-07-01 16:49:20 +0000

    api: declaration des methodes du crud de matiere effectué

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from api.views.etudiant_api_views import etudiant_list
+from api.views.matiere_api_views import list_matiere,create_matiere,update_matiere,delete_matiere,detail_matiere
 
 urlpatterns = [
-    path("etudiant/", etudiant_list)
+    path("etudiant/", etudiant_list),
+    path("matieres/", list_matiere),
+    path("matiere/create/<int:id>/", create_matiere),
+    path("matiere/update/<int:id>/", update_matiere),
+    path("matiere/delete/<int:id>/", delete_matiere),
+    path("matiere/detail/<int:id>/", detail_matiere)
 ]
