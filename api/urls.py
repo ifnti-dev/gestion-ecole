486c9b3c5f64ab1b1dbb663f82b4a43b68a5f606
dimitri <devgtfod@gmail.com>
2024-07-01 16:10:10 +0000

    api ue

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from api.views.etudiant_api_views import etudiant_list
+from api.views import ue_api_views
+
+
 
 urlpatterns = [
-    path("etudiant/", etudiant_list)
+    path("etudiant/", etudiant_list),
+
+    #-----api des ues------------
+    path("ues/", ue_api_views.list_ue),
+    path("ue/create", ue_api_views.create_ue),
+    path("ue/update/<int:pk>", ue_api_views.update_ue),
+    path("ue/deltail/<int:pk>", ue_api_views.detail_ue),
+    path("ue/delete/<int:pk>", ue_api_views.delete_ue),
 ]
