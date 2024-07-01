dbcf732ea86771be250811dfd6bf96cd26765a8e
tchabana <hafiztchabana2004@gmail.com>
2024-07-01 11:38:31 +0000

    refactoring de l'api

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from api.views.etudiant.etudiant_api_views import etudiant_list
+from api.views.etudiant_api_views import etudiant_list
 
 urlpatterns = [
     path("etudiant/", etudiant_list)
