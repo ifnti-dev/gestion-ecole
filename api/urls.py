055f8b034e2bab388177b49ed256a31020d06b82
Cherifa OGBONE INOUSSA <cogboneinoussa@gmail.com>
2024-07-02 10:09:28 +0000

    update des urls api de personnel

--- api/urls.py
+++ api/urls.py
@@ -1,21 +1,21 @@
 from django.urls import path
 from api.views.etudiant_api_views import etudiant_list
 from api.views import ue_api_views
-from api.views.personnel_api_views import *
+from api.views import personnel_api_views
 
 urlpatterns = [
     path("etudiant/", etudiant_list),
 
 
     ############### Personnels urls###################################
-    path("personnels",list_personnel),
-    path("personnel/create",create_personnel),
-    path("personnel/delete/<int:pk>",delete_personnel),
-    path("personnel/update/<int:pk>",update_personnel),
-    path("personnel/detail/<int:pk>",detail_personnel),
-    path("personnel/form__demander_conge",form_demander_conge),
-    path("personnel/imprimer_demande_conge", imprimer_demande_conge),
-    path("personnel/accorder_conge", accorder_conge),
+    path("personnels",personnel_api_views.list_personnel),
+    path("personnel/create",personnel_api_views.create_personnel),
+    path("personnel/delete/<int:pk>",personnel_api_views.delete_personnel),
+    path("personnel/update/<int:pk>",personnel_api_views.update_personnel),
+    path("personnel/detail/<int:pk>",personnel_api_views.detail_personnel),
+    path("personnel/form__demander_conge",personnel_api_views.form_demander_conge),
+    path("personnel/imprimer_demande_conge",personnel_api_views.imprimer_demande_conge),
+    path("personnel/accorder_conge",personnel_api_views.accorder_conge),
 
     #-----api des ues------------
     path("ues/", ue_api_views.list_ue),
